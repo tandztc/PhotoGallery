8cd7cc32f33250f7ad09a7f953a15237d0c32001
tanyong02 <tanyong02@baidu.com>
2016-02-23 14:37:52 +0800

    创建工程

---
+++ app/src/main/java/com/tanyong/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,12 @@
+package com.tanyong.photogallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
