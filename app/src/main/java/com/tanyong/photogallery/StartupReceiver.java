5486550d050883666a280b0ea789bccba4141eb7
tandztc <tandztc@yeah.net>
2016-03-01 12:15:57 +0800

    添加接受系统启动完成消息的功能

---
+++ app/src/main/java/com/tanyong/photogallery/StartupReceiver.java
@@ -0,0 +1,20 @@
+package com.tanyong.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by baidu on 16/3/1.
+ * 接收开机启动成功的消息
+ */
+public class StartupReceiver extends BroadcastReceiver {
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast intent: " + intent);
+    }
+
+}
