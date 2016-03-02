c6ba22f7635b2e067e49774004a1e868c0ee013f
tandztc <tandztc@yeah.net>
2016-03-02 11:05:57 +0800

    增加网页载入时的进度条显示和副标题显示

--- app/src/main/java/com/tanyong/photogallery/PhotoPageFragment.java
+++ app/src/main/java/com/tanyong/photogallery/PhotoPageFragment.java
@@ -4,11 +4,14 @@ package com.tanyong.photogallery;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 /**
@@ -22,6 +25,7 @@ public class PhotoPageFragment extends VisibleFragment {
 
     private Uri mUri;
     private WebView mWebView;
+    private ProgressBar mProgressBar;
 
 
     public PhotoPageFragment() {
@@ -56,14 +60,33 @@ public class PhotoPageFragment extends VisibleFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_photo_page, container, false);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.fragment_photo_page_progress_bar);
+        mProgressBar.setMax(100);
         mWebView = (WebView) view.findViewById(R.id.fragment_photo_page_web_view);
         mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView webView, int newProgress) {
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+
+            public void onReceivedTitle(WebView webView, String title) {
+                AppCompatActivity activity = (AppCompatActivity) getActivity();
+                activity.getSupportActionBar().setSubtitle(title);
+            }
+        });
         mWebView.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView webView, String url) {
                 return false;
             }
         });
         mWebView.loadUrl(mUri.toString());
+
+
         return view;
     }
 
